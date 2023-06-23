dbdb29c0aa8e6ef142fdfb2adbd8bb2eb995c4a0
Jonathan Sebastian Pastas Pastaz <sebas30sebastian@gmail.com>
2023-06-23 10:07:20 -0500

    config init

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 
-print("Hola Mundo")
+from flask import Flask,render_template
+
+app = Flask(__name__)
+app.secret_key = 'tid@205'
+
+@app.route('/')
+
+def formulario() -> 'html':
+  return render_template('index.html',titulo ='Inicio')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
