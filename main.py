dbb151001bf45f38c0ad5a304d6a5a134a947c13
Jonathan Sebastian Pastas Pastaz <sebas30sebastian@gmail.com>
2023-06-26 15:33:34 -0500

    carga de diseño web
    se cargo el diseño web de la parte de invitado (vista al momento de cargar la web)

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ from flask import Flask,render_template
 app = Flask(__name__)
 app.secret_key = 'tid@205'
 
+@app.route('/pr')
+
+def prueba() -> 'html':
+  return render_template('pr.html',titulo ='Inicio')
+
 @app.route('/')
 
 def formulario() -> 'html':
-  return render_template('index.html',titulo ='Inicio')
+  return render_template('invitado/index.html',titulo ='Inicio')
 
+@app.route('/login')
+
+def login() -> 'html':
+  return render_template('invitado/login.html',titulo ='Iniciar Sesion')
+
+@app.route('/registro')
+
+def registro() -> 'html':
+  return render_template('invitado/registro.html',titulo ='Registro')
 
 if __name__ == '__main__':
     app.run(debug=True)
